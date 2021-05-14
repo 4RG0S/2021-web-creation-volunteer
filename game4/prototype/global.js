0b714f43b287ba40e42f4fef88ffdc58a03d4ec0
Begachu <asdpoi013@gmail.com>
2021-05-14 21:41:50 +0900

    이미지에 대한 상수 추가

--- game4/prototype/global.js
+++ game4/prototype/global.js
@@ -6,6 +6,9 @@ const LIMIT_TIME = 90;
 const CARD_ID = "#card_";
 const SELECT_CARD_COLOR = "#ff0000";
 const BACK_OF_CARD_COLOR = "#000000";
+const FRONT_OF_CARD_IMG = ["../img/img_1.png","../img/img_2.png","../img/img_3.png","../img/img_4.png","../img/img_5.png",
+"../img/img_6.png","../img/img_7.png","../img/img_8.png","../img/img_9.png","../img/img_10.png","../img/img_11.png","../img/img_12.png"];
+const BACK_OF_CARD_IMG = "../img/back_img.png";
 const TIMER_TEXT = document.querySelector('#timer');
 const SCORE_BOARD = document.querySelector('#score');
 
