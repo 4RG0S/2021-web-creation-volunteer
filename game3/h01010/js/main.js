88bcc574225daeef74aa36e236b0611eccfe5a71
h01010 <seoyeonju1198@gmail.com>
2021-07-28 18:16:30 +0900

    Add game3-bookGame

--- game3/h01010/js/main.js
+++ game3/h01010/js/main.js
@@ -1,5 +1,5 @@
 unmuteButton.addEventListener('click', function() {
-    video.volume = 0.2;
+    video.volume = 0.5;
     video.muted = false;
     setTimeout(function() {
         document.body.style.background = "url('https://images.unsplash.com/photo-1514483127413-f72f273478c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80') no-repeat 0 0";
@@ -10,9 +10,9 @@ unmuteButton.addEventListener('click', function() {
 });
 
 scaryUnmuteButton.addEventListener('click', function() {
-    video.volume = 0.2;
-    scaryVideo.volume = 0.2;
-
+    video.volume = 0.5;
+    scaryVideo.volume = 0.5;
+    
     video.muted = false;
     scaryVideo.muted = false;
     setTimeout(function() {
